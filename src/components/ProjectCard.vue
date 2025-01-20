54848348dadc3b92ee606e1d959f0c25801c575e
churuizramos <ruizramos04@outlook.com>
2025-01-20 18:31:03 -0500

    started the project cards component, created the projects section for homeview

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+    pTitle: String,
+    description: String,
+    pLink: String,
+    repoName: String,
+    cover: String
+})
+</script>
+
+<template>
+    <div class="border-2 border-black rounded-2xl p-4 lg:w-80 sm:w-full">
+        <a href="" target="_blank">
+            <h1 class="text-xl font-bold">{{ pTitle }}</h1>
+            <h1>{{ description }}</h1>
+            <h1>{{ repoName }}</h1>
+        </a>
+    </div>
+</template>
---
+++ src/components/ProjectsSection.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ProjectCard from './ProjectCard.vue';
+</script>
+
+<template>
+    <div class="bg-white px-20 py-10 ">
+        <h1 class="text-3xl font-bold text-center">Projects</h1>
+        <section>
+            <ProjectCard
+                pTitle = "Project Title"
+                description = "Project Desc"
+                pLink = https://github.com/churuizramos/home-page
+                repoName = "home-page"
+            />
+        </section>
+    </div>
+</template>
