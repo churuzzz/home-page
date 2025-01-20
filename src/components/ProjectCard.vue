d40e9b56d642a382eaf7063c593a3a7691df990a
churuizramos <ruizramos04@outlook.com>
2025-01-20 18:45:03 -0500

    added a placeholder image, fixed link styling

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,11 +9,14 @@ defineProps({
 </script>
 
 <template>
-    <div class="border-2 border-black rounded-2xl p-4 lg:w-80 sm:w-full">
+    <div class="border-2 border-black rounded-2xl lg:w-80 sm:w-full">
         <a href="" target="_blank">
-            <h1 class="text-xl font-bold">{{ pTitle }}</h1>
-            <h1>{{ description }}</h1>
-            <h1>{{ repoName }}</h1>
+            <img src="https://placehold.co/600x400?text=Hello+World" alt="placeholder" class="rounded-t-2xl"/>
+            <div class="p-4">
+                <h1 class="text-xl font-bold">{{ pTitle }}</h1>
+                <h1>{{ description }}</h1>
+                <h1>{{ repoName }}</h1>
+            </div>
         </a>
     </div>
 </template>
