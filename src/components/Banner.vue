a5d8e778efbe48ae87dd16be3c89944ab8a971f4
churuzzz <ruizramos04@outlook.com>
2025-01-27 18:52:21 -0500

    color and format changes, conditional styling for mobile

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,17 +1,34 @@
 <template>
-    <section class="bg-gradient-to-b to-steel_blue-300 from-emerald-600 px-20 py-40 flex flex-row justify-between">
-        <div>
-            <h1 class="font-bold text-8xl">JESUS RUIZ RAMOS</h1>
-            <div class="flex flex-row gap-4 font-bold">
-                <h1>churuizramos</h1>
-                <h1>churuzz</h1>
-            </div>
-        </div>
-        <div class="m-8 p-3 rounded-full flex flex-row bg-white">
-            <a href="https://github.com/churuizramos" target="_blank"><img src="../assets/icons8-github-48.png" alt="github link"/></a>
-            <a href="https://www.linkedin.com/in/jesus-ruiz-ramos-9591a5238/" target="_blank"><img src="../assets/icons8-linkedin-48.png" alt="linkedin link"/></a>
-            <a href="https://bsky.app/profile/churuizramos.bsky.social" target="_blank"><img src="../assets/icons8-bluesky-48.png" alt="bluesky link"/></a>
-            <a href="https://www.instagram.com/churuizramos/" target="_blank"><img src="../assets/icons8-instagram-48.png" alt="instagram link"/></a>
+    <section
+        class="bg-slate-300 px-20 py-40 flex flex-col justify-between gap-4 content-center"
+    >
+        <h1 class="font-bold lg:text-8xl text-6xl text-center">CHURUZZZ</h1>
+        <div class="m-8 p-3 rounded-full flex flex-row bg-white m-auto">
+            <a href="https://github.com/churuizramos" target="_blank"
+                ><img
+                    class="link_hover"
+                    src="../assets/icons8-github-48.png"
+                    alt="github link"
+            /></a>
+            <a
+                href="https://bsky.app/profile/churuizramos.bsky.social"
+                target="_blank"
+                ><img
+                    class="link_hover"
+                    src="../assets/icons8-bluesky-48.png"
+                    alt="bluesky link"
+            /></a>
+            <a href="https://www.instagram.com/churuizramos/" target="_blank"
+                ><img
+                    class="link_hover"
+                    src="../assets/icons8-instagram-48.png"
+                    alt="instagram link"
+            /></a>
         </div>
     </section>
 </template>
+<style scoped>
+.link_hover {
+    @apply transition-all hover:bg-stone-500 rounded-full;
+}
+</style>
