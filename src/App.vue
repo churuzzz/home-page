6d098e044224ee294a6f272178f7886c7589ecb9
churuzzz <ruizramos04@outlook.com>
2025-01-27 19:55:31 -0500

    idk what changed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
-import { RouterView, RouterLink } from 'vue-router';
+import { RouterView, RouterLink } from "vue-router";
 </script>
 
 <template>
-  <nav class="flex flex-row gap-10 p-5 text-xl bg-steel_blue-500 text-white">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/contact">Contact</RouterLink>
-  </nav>
-  <main class="bg-steel_blue-400">
-    <RouterView />
-  </main>
+    <nav
+        class="flex flex-row gap-10 p-5 text-xl bg-steel_blue-500 text-white justify-center"
+    >
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+    </nav>
+    <main class="bg-steel_blue-400">
+        <RouterView />
+    </main>
 </template>
